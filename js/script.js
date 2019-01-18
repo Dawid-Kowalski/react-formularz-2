d51adf227a1231dcd83e0178ef0c7d64b7f5399b
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-01-18 11:21:49 +0100

    Add motor and motorlist component

--- js/script.js
+++ js/script.js
@@ -8,11 +8,73 @@ class Title extends React.Component {
 	}
 }
 
+class Motor extends React.Component {
+	render() {
+		return (
+			<tbody>
+				{this.props.motors.map(motor =>
+					<tr>
+						<td>{motor.powerSupply}</td>
+						<td>{motor.powerConsuption}</td>
+						<td>{motor.rotationSpeed}</td>
+					</tr>
+				)}
+			</tbody>
+		)
+	}
+}
+
+class MotorsList extends React.Component {
+	render() {
+		return (
+			<div>
+				<table>
+					<thead>
+						<tr>
+							<td>napięcie zasialania</td>
+							<td>pobór prądu</td>
+							<td>prędkość obrotowa</td>
+						</tr>
+					</thead>
+					<Motor motors={this.props.motors} />
+				</table>
+			</div>
+		)
+	}
+}
+
+
 class App extends React.Component {
+
+	motors = [
+		{
+			id: 1,
+			powerSupply: 5,
+			powerConsuption: 180,
+			rotationSpeed: 80
+		},
+		{
+			id: 2,
+			powerSupply: 6,
+			powerConsuption: 30,
+			rotationSpeed: 90
+		},
+		{
+			id: 3,
+			powerSupply: 7,
+			powerConsuption: 50,
+			rotationSpeed: 10300
+		},
+
+	]
+
+
+
 	render() {
 		return (
 			<div>
 				<Title title="formularz"/>
+				<MotorsList motors={this.motors} />
 			</div>
 		)
 	}
