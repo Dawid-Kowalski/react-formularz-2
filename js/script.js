57c2edc42b91607a89b05148b27ce30056fe1aeb
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2019-01-18 12:32:09 +0100

    Add functions to form

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+class NewMotor {
+	constructor(id, powerSupply, powerConsumption, speedRotation) {
+		this.id = id;
+		this.powerSupply = powerSupply;
+		this.powerConsumption = powerConsumption;
+		this.speedRotation = speedRotation;
+	}
+}
+
+
 class Title extends React.Component {
 	render() {
 		return (
@@ -10,103 +20,142 @@ class Title extends React.Component {
 
 
 class Form extends React.Component {
+
+	state = {
+		motors: this.props.motors,
+		newId: 3,
+		newPowerSupply: 'brak',
+		newPowerConsumption: 'brak',
+		newSpeedRotation: 'brak'
+	}
+
+	changePowerSupply = event => {
+		this.setState({newPowerSupply: event.target.value});
+	}
+
+	changePowerConsumption = event => {
+		this.setState({newPowerConsumption: event.target.value});
+	}
+
+	changeSpeedRotation = event => {
+		this.setState({newSpeedRotation: event.target.value});
+	}
+
+	test = event => {
+		event.preventDefault();
+		this.state.newId++;
+		let newMotorsList = this.state.motors;
+		const newMotor = new NewMotor (this.state.newId, this.state.newPowerSupply, this.state.newPowerConsumption, this.state.newSpeedRotation);
+		newMotorsList.push(newMotor);
+		this.setState({motors: newMotorsList});
+		this.setState({newPowerSupply: 'brak'});
+		this.setState({newPowerConsumption: 'brak'});
+		this.setState({newSpeedRotation: 'brak'});
+	}
+
 	render() {
 		return (
-			<form>
-				<label>napięcie zasialania</label>
-				<select>
-					<option value='1'>1</option>
-					<option value='1.5'>1.5</option>
-					<option value='3'>3</option>
-					<option value='3.7'>3.7</option>
-					<option value='4'>4</option>
-					<option value='4.5'>4.5</option>
-					<option value='5'>5</option>
-					<option value='6'>6</option>
-					<option value='9'>9</option>
-					<option value='12'>12</option>
-				</select>
-				<label>pobór prądu</label>
-				<select>
-					<option value='22'>22</option>
-					<option value='30'>30</option>
-					<option value='35'>35</option>
-					<option value='40'>40</option>
-					<option value='45'>45</option>
-					<option value='50'>50</option>
-					<option value='60'>60</option>
-					<option value='70'>70</option>
-					<option value='80'>80</option>
-					<option value='94'>94</option>
-					<option value='100'>100</option>
-					<option value='120'>120</option>
-					<option value='125'>125</option>
-					<option value='130'>130</option>
-					<option value='150'>150</option>
-					<option value='170'>170</option>
-					<option value='180'>180</option>
-					<option value='190'>190</option>
-					<option value='200'>200</option>
-					<option value='250'>250</option>
-					<option value='275'>275</option>
-					<option value='300'>300</option>
-					<option value='350'>350</option>
-					<option value='400'>400</option>
-					<option value='450'>450</option>
-					<option value='500'>500</option>
-					<option value='550'>550</option>
-					<option value='750'>750</option>
-					<option value='770'>770</option>
-					<option value='800'>800</option>
-					<option value='1350'>1350</option>
-					<option value='1850'>1850</option>
-					<option value='4480'>4480</option>
-				</select>
-				<label>prędkość obrotowa</label>
-				<select>
-					<option value='12'>12</option>
-					<option value='14'>14</option>
-					<option value='15'>15</option>
-					<option value='25'>25</option>
-					<option value='30'>30</option>
-					<option value='32'>32</option>
-					<option value='33'>33</option>
-					<option value='40'>40</option>
-					<option value='45'>45</option>
-					<option value='46'>46</option>
-					<option value='50'>50</option>
-					<option value='57'>57</option>
-					<option value='60'>60</option>
-					<option value='65'>65</option>
-					<option value='75'>75</option>
-					<option value='80'>80</option>
-					<option value='85'>85</option>
-					<option value='90'>90</option>
-					<option value='100'>100</option>
-					<option value='120'>120</option>
-					<option value='130'>130</option>
-					<option value='133'>133</option>
-					<option value='135'>135</option>
-					<option value='140'>140</option>
-					<option value='150'>150</option>
-					<option value='160'>160</option>
-					<option value='165'>165</option>
-					<option value='170'>170</option>
-					<option value='180'>180</option>
-					<option value='185'>185</option>
-					<option value='200'>200</option>
-					<option value='210'>210</option>
-					<option value='220'>220</option>
-					<option value='225'>225</option>
-					<option value='230'>230</option>
-					<option value='250'>250</option>
-					<option value='275'>275</option>
-					<option value='285'>285</option>
-					<option value='290'>290</option>
-					<option value='294'>294</option>
-				</select>
-				<button>dodaj</button>
-			</form>
+			<div>
+				<form onSubmit={this.test}>
+					<label>napięcie zasialania</label>
+					<select value={this.state.newPowerSupply} onChange={this.changePowerSupply}>
+						<option value='brak'>brak</option>
+						<option value='1'>1</option>
+						<option value='1.5'>1.5</option>
+						<option value='3'>3</option>
+						<option value='3.7'>3.7</option>
+						<option value='4'>4</option>
+						<option value='4.5'>4.5</option>
+						<option value='5'>5</option>
+						<option value='6'>6</option>
+						<option value='9'>9</option>
+						<option value='12'>12</option>
+					</select>
+					<label>pobór prądu</label>
+					<select value={this.state.newPowerConsumption} onChange={this.changePowerConsumption}>
+						<option value='brak'>brak</option>
+						<option value='22'>22</option>
+						<option value='30'>30</option>
+						<option value='35'>35</option>
+						<option value='40'>40</option>
+						<option value='45'>45</option>
+						<option value='50'>50</option>
+						<option value='60'>60</option>
+						<option value='70'>70</option>
+						<option value='80'>80</option>
+						<option value='94'>94</option>
+						<option value='100'>100</option>
+						<option value='120'>120</option>
+						<option value='125'>125</option>
+						<option value='130'>130</option>
+						<option value='150'>150</option>
+						<option value='170'>170</option>
+						<option value='180'>180</option>
+						<option value='190'>190</option>
+						<option value='200'>200</option>
+						<option value='250'>250</option>
+						<option value='275'>275</option>
+						<option value='300'>300</option>
+						<option value='350'>350</option>
+						<option value='400'>400</option>
+						<option value='450'>450</option>
+						<option value='500'>500</option>
+						<option value='550'>550</option>
+						<option value='750'>750</option>
+						<option value='770'>770</option>
+						<option value='800'>800</option>
+						<option value='1350'>1350</option>
+						<option value='1850'>1850</option>
+						<option value='4480'>4480</option>
+					</select>
+					<label>prędkość obrotowa</label>
+					<select value={this.state.newSpeedRotation} onChange={this.changeSpeedRotation}>
+					<option value='brak'>brak</option>
+						<option value='12'>12</option>
+						<option value='14'>14</option>
+						<option value='15'>15</option>
+						<option value='25'>25</option>
+						<option value='30'>30</option>
+						<option value='32'>32</option>
+						<option value='33'>33</option>
+						<option value='40'>40</option>
+						<option value='45'>45</option>
+						<option value='46'>46</option>
+						<option value='50'>50</option>
+						<option value='57'>57</option>
+						<option value='60'>60</option>
+						<option value='65'>65</option>
+						<option value='75'>75</option>
+						<option value='80'>80</option>
+						<option value='85'>85</option>
+						<option value='90'>90</option>
+						<option value='100'>100</option>
+						<option value='120'>120</option>
+						<option value='130'>130</option>
+						<option value='133'>133</option>
+						<option value='135'>135</option>
+						<option value='140'>140</option>
+						<option value='150'>150</option>
+						<option value='160'>160</option>
+						<option value='165'>165</option>
+						<option value='170'>170</option>
+						<option value='180'>180</option>
+						<option value='185'>185</option>
+						<option value='200'>200</option>
+						<option value='210'>210</option>
+						<option value='220'>220</option>
+						<option value='225'>225</option>
+						<option value='230'>230</option>
+						<option value='250'>250</option>
+						<option value='275'>275</option>
+						<option value='285'>285</option>
+						<option value='290'>290</option>
+						<option value='294'>294</option>
+					</select>
+					<button>dodaj</button>
+				</form>
+				<MotorsList motors={this.state.motors}/>
+			</div>
 		)
 	}
 }
@@ -118,8 +167,8 @@ class Motor extends React.Component {
 				{this.props.motors.map(motor =>
 					<tr>
 						<td>{motor.powerSupply}</td>
-						<td>{motor.powerConsuption}</td>
-						<td>{motor.rotationSpeed}</td>
+						<td>{motor.powerConsumption}</td>
+						<td>{motor.speedRotation}</td>
 					</tr>
 				)}
 			</tbody>
@@ -153,20 +202,20 @@ class App extends React.Component {
 		{
 			id: 1,
 			powerSupply: 5,
-			powerConsuption: 180,
-			rotationSpeed: 80
+			powerConsumption: 180,
+			speedRotation: 80
 		},
 		{
 			id: 2,
 			powerSupply: 6,
-			powerConsuption: 30,
-			rotationSpeed: 90
+			powerConsumption: 30,
+			speedRotation: 90
 		},
 		{
 			id: 3,
 			powerSupply: 6,
-			powerConsuption: 50,
-			rotationSpeed: 10300
+			powerConsumption: 50,
+			speedRotation: 10300
 		},
 
 	]
@@ -177,8 +226,7 @@ class App extends React.Component {
 		return (
 			<div>
 				<Title title="formularz"/>
-				<Form />
-				<MotorsList motors={this.motors} />
+				<Form motors={this.motors}/>
 			</div>
 		)
 	}
